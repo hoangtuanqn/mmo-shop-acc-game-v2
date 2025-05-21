12e9083fd11d7e6cc9389428e0b669fb5c52660e
hoangtuanqn <phamhoangtuanqn@gmail.com>
2025-05-21 12:07:59 +0700

    ui: add page login

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { createInertiaApp } from "@inertiajs/react";
 import { createRoot } from "react-dom/client";
+import MasterLayout from "./layout/MasterLayout";
 
 createInertiaApp({
     title: (title) => `${title} - TUANORI.VN`,
     resolve: (name) => {
         const pages = import.meta.glob("./Pages/**/*.jsx", { eager: true });
-        return pages[`./Pages/${name}.jsx`];
+        let page = pages[`./Pages/${name}.jsx`];
+        page.default.layout = page.default.layout || ((page) => <MasterLayout>{page}</MasterLayout>);
+        return page;
     },
     setup({ el, App, props }) {
         createRoot(el).render(<App {...props} />);
--- resources/js/utils/functions.js
+++ resources/js/utils/functions.js
@@ -1,7 +1,20 @@
+import Swal from "sweetalert2";
+
 export function formatNumber(amount) {
     return new Intl.NumberFormat("en-US").format(amount);
 }
 export function subString(string) {
     const length = string.length;
     return string.substring(0, length - 3) + "***";
+}
+export function showAlert(status = "success", message) {
+    const title = {
+        success: "Thành công!",
+        error: "Có lỗi!",
+    };
+    return Swal.fire({
+        title: title[status],
+        text: message,
+        icon: status,
+    });
 }
